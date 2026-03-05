72281e2296c42086462db4246b317cf53282e9a1
Alex Curtaud <alex.curtaud@gmail.com>
2026-03-05 16:42:51 +0100

    Javadoc - Create Javadoc - Warrior/Wizard

---
+++ src/characters/Warrior.java
@@ -0,0 +1,43 @@
+package characters;
+
+public class Warrior extends Character {
+    private int health = 10;
+    private int physicalAttack = 5;
+
+    /**
+     * Warrior is extension of the Character Class. A Warrior is a type of Character.
+     * @param characterName
+     * @param type
+     */
+    public Warrior(String characterName, String type) {
+        super(characterName, type);
+    }
+
+    // GETTER
+    /**
+     * Get the HealthStat
+     * @return health
+     */
+    @Override
+    public int getHealthStat() {
+        return health;
+    }
+
+    /**
+     * Get the AttackStat
+     * @return attack
+     */
+    @Override
+    public int getAttackStat() {
+        return physicalAttack;
+    }
+
+    /**
+     * Gather the Warrior information
+     * @return it to be printed
+     */
+    @Override
+    public String toString() {
+        return "Player : \n" + name + " " + type + "\n" + "Health [" + health + "] + PhysicalAttack [" + physicalAttack + "]";
+    }
+}
---
+++ src/characters/Wizard.java
@@ -0,0 +1,43 @@
+package characters;
+
+public class Wizard extends Character {
+    private final int health = 6;
+    private final int magicPower = 8;
+
+    /**
+     * Wizard is extension of the Character Class. A Wizard is a type of Character.
+     * @param characterName
+     * @param type
+     */
+    public Wizard(String characterName, String type) {
+        super(characterName, type);
+    }
+
+    // GETTER
+    /**
+     * Get the HealthStat
+     * @return health
+     */
+    @Override
+    public int getHealthStat() {
+        return health;
+    }
+
+    /**
+     * Get the AttackStat
+     * @return magicPower
+     */
+    @Override
+    public int getAttackStat() {
+        return magicPower;
+    }
+
+    /**
+     * Gather the Wizard information
+     * @return it to be printed
+     */
+    @Override
+    public String toString() {
+        return "Player : \n" + name + " " + type + "\n" + "Health [" + health + "] MagicPower [" + magicPower + "]";
+    }
+}
