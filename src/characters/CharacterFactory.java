593e43b913c229aab949ef0494d1601e6affbe28
Alex Curtaud <alex.curtaud@gmail.com>
2026-03-05 16:45:34 +0100

    StandBy CharacterFactory

--- src/characters/CharacterFactory.java
+++ src/characters/CharacterFactory.java
@@ -1,9 +1,9 @@
-package Characters;
+package characters;
 
 public class CharacterFactory {
-    public static Character characterCreator(String name, int type) {
+    public static Character characterCreator(String name, String type) {
         Character character;
-        if(type == 1) {
+        if(type == "Wizard") {
             character = new Wizard(name, type);
             System.out.println("Player " + name + ", join the wizard guild");
         } else {
