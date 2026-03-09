f8ee75f2f9e0b3b3e0c565aa4a90d10b77e59a88
Alex Curtaud <alex.curtaud@gmail.com>
2026-03-09 16:49:06 +0100

    GamePlay package: - List of Cell with ArrayList - Instance created and tested

--- src/Gameplay/Board.java
+++ src/Gameplay/Board.java
@@ -1,5 +1,9 @@
 package Gameplay;
 
+import DefensiveEquipment.Potion;
+import Enemy.Gobelin;
+import OffensiveEquipment.Weapon;
+
 import java.util.ArrayList;
 
 public class Board {
@@ -12,6 +16,7 @@ public class Board {
      */
     public Board(int cellNb) {
         this.cellNb = cellNb;
+        this.cells = new ArrayList<Cell>();
     }
 
     // GETTER
@@ -30,9 +35,12 @@ public class Board {
      * It creates a Cell Object has many times as the cellNb attribute of the board.
      * @return board of Cell
      */
-    public Cell[] initBoard() {
-        cells[0] = new Cell[cellNb];
-
+    public ArrayList<Cell> initBoard() {
+        cells.add(0, new Cell(1, null, null, null));
+        cells.add(1, new Cell(2, new Gobelin("Gobelin", 9, "Gobelin"), null, null));
+        cells.add(2, new Cell(3, null, new Weapon("Basic Sword", "Common", "Weapon", 5, "Sword"), null));
+        cells.add(3, new Cell(4, null, null, new Potion("Standard Potion", "Common", "Health", 2)));
+        return cells;
     }
 
 
--- src/Gameplay/Cell.java
+++ src/Gameplay/Cell.java
@@ -1,19 +1,40 @@
 package Gameplay;
 
+import DefensiveEquipment.DefensiveEquipment;
+import Enemy.Enemy;
+import OffensiveEquipment.OffensiveEquipment;
+import Characters.Character;
+
 public class Cell {
     private int id;
-    private Ennemy ennemy;
-    private MysteryBox mysteryBox = null;
+    private Character player = null;
+    private Enemy enemy;
+    private OffensiveEquipment offEquip = null;
+    private DefensiveEquipment defEquip = null;
+//    private MysteryBox mysteryBox = null;
 
-    public Cell(int id, Ennemy ennemy, MysteryBox mysteryBox) {
+    /**
+     * Cell Class and Constructor are made to create instance of a Board Cell. In a boardgame, there is a number of Cells and each cell has a content.
+     * Here a cell must have an id, and it can have a Enemy, an OffensiveEquipment and a DefensiveEquipement
+     *
+     * @param id
+     * @param enemy
+     * @param offEquip
+     * @param defEquip
+     */
+    public Cell(int id, Enemy enemy, OffensiveEquipment offEquip, DefensiveEquipment defEquip) {
         this.id = id;
-        this.ennemy = ennemy;
-        this.mysteryBox = mysteryBox;
+        this.enemy = enemy;
+        this.offEquip = offEquip;
+        this.defEquip = defEquip;
+//        this.mysteryBox = mysteryBox;
     }
 
     // GETTER
+
     /**
      * Get Cell id
+     *
      * @return id
      */
     public int getId() {
@@ -21,18 +42,73 @@ public class Cell {
     }
 
     /**
-     * Get Cell Ennemy
-     * @return ennemy
+     * Get the Cell Character
+     *
+     * @return Character
      */
-    public Ennemy getEnnemy() {
-        return ennemy;
+    public Character getPlayer() {
+        return player;
+    }
+
+    /**
+     * Get Cell Enemy
+     *
+     * @return enemy
+     */
+    public Enemy getEnemy() {
+        return enemy;
+    }
+
+    /**
+     * Get the Cell OffensiveEquipment
+     *
+     * @return OffensiveEquipement
+     */
+    public OffensiveEquipment getOffEquip() {
+        return offEquip;
+    }
+
+    /**
+     * Get the Cell DefensiveEquipment
+     *
+     * @return DefensiveEquipement
+     */
+    public DefensiveEquipment getDefEquip() {
+        return defEquip;
     }
 
     /**
      * Get Cell MysteryBox
+     *
      * @return mysteryBox
      */
-    public MysteryBox getMysteryBox() {
-        return mysteryBox;
+
+//    public MysteryBox getMysteryBox() {
+//        return mysteryBox;
+//    }
+
+    //SETTER
+    public void setPlayer(Character player) {
+        if (player.getCurrentPosition() == this.id) {
+            this.player = player;
+        }
+        player = null;
     }
-}
+
+    //toString
+    @Override
+    public String toString() {
+        String sentence = "Empty Cell";
+        if (enemy != null) {
+            sentence = "\nCell " + this.getId() + "\nYou encounter: " + enemy.toString();
+        }
+        if (offEquip != null) {
+                sentence = "\nCell " + this.getId() + "\nYou found: " + offEquip.toString();
+            }
+        if (defEquip != null) {
+                sentence = "\nCell " + this.getId() + "\nYou found: " + defEquip.toString();
+        }
+        return sentence;
+        }
+    }
+
